31a1ecea546e93825fb042d9edf7cafd9fd29e7d
mounishvatti <mounishvatti2002@gmail.com>
2024-01-31 22:52:17 +0530

    added new files

--- test.cpp
+++ test.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h> 
 using namespace std;
 int main(){
-    array<int, 5> arr = {1,3,5};
 
-    for(auto it: arr){
-        cout << it << " ";
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){ // taking the array
+        cin >> arr[i];
     }
+
+    int maxsum = INT_MIN;
+    for(int i=0; i<n; i++){
+        int sum = 0;
+        for(int j=i; j<n; j++){
+            sum+=arr[j];
+            maxsum = max(maxsum,sum);      
+        }
+    }
+    cout << maxsum;
+    return 0;
 }
