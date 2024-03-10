704f469cccb799a9ecbbee25d3d08ebe7a3a496b
mounishvatti <mounishvatti2002@gmail.com>
2024-03-10 14:12:15 +0530

    added new concepts

---
+++ OOPs in C++/encapsulation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Car{
+    private: // if all the data members are in private then it is called fully encapsulated 
+    string colour;
+    string brand;
+    int yom;
+};
+
+int main(){
+    Car c1;
+
+    return 0;
+}
---
+++ OOPs in C++/inheritance.cpp
@@ -0,0 +1,150 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Human{
+public:
+    int age;
+    int weight;
+    int height;
+
+    void setAge(int age){
+        this->age = age;
+    }
+
+    void setWeight(int weight){
+        this->weight = weight;
+    }
+
+    void setHeight(int height){
+        this->height = height;
+    }
+
+};
+
+// single inheritance
+class Male : public Human{  // cat inherits the properties of animal
+    public:
+    string name;
+
+    void setName(string name){
+        this->name = name;
+    }
+
+};
+
+// multi-level inheritance
+
+// class A{};
+
+// class B:public A{};
+
+// class C:public B{};
+
+
+// hierarchical inheritance
+class A{};
+
+class B:public A{};
+
+class C:public A{};
+
+class D:public B{};
+
+class E:public B{};
+
+
+
+class Animal{
+    public:
+    int age;
+    int weight;
+    int height;
+
+    void bark(){
+        cout << "Barking Sound" << endl;
+    }
+};
+
+class Homosapians{
+    public:
+    void speak(){
+        cout << "Speaking sound" << endl;
+    }
+};
+
+class exp1{
+    public:
+
+    void sex(){
+        cout << "69" << endl;
+    }
+};
+
+// multiple inheritance
+class Hybrid: public Animal, public  Homosapians, public exp1 {
+
+};  
+
+// inheritance ambiguity
+class Parent1{
+    public:
+    void print(){
+        cout << "Parent1" << endl;
+    }
+};
+
+class Parent2{
+    public:
+    void print(){
+        cout << "Parent2" << endl;
+    }
+};
+
+class child1:public Parent1, public Parent2{
+
+};
+
+// method overriding
+class Parent{
+    public:
+    void speak(){
+        cout << "I can speak" << endl;
+    }
+};
+
+class child:public Parent{
+    public:
+    void speak(){
+        cout << "I can't speak" << endl;
+    }
+};
+
+int main(){
+
+    // Male male1;   // object creation
+
+    // male1.age = 28;
+    // male1.weight = 65;
+    // male1.height = 165;
+    // male1.name = "Mounish";
+
+    // //or
+
+    // Male male2;
+    // male2.setAge(21);
+    // male2.setWeight(69);
+    // male2.setHeight(167);
+    // male2.setName("Rahul");
+
+    // Hybrid obj1;
+    // obj1.sex();
+
+    child1 obj;
+    obj.Parent1::print();
+    obj.Parent2::print();
+
+    child object;
+
+    object.speak();
+    return 0;
+}
