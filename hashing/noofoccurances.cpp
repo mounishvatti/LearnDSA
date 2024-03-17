2841053aa0175d736385a1031b9f31abf4a1aef5
mounishvatti <mounishvatti2002@gmail.com>
2024-03-17 14:56:40 +0530

    updated code

--- hashing/noofoccurances.cpp
+++ hashing/noofoccurances.cpp
@@ -1,44 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-
-//brute force approach
-
-int f(int arr[], int num)
-{
-    int count = 0;
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (arr[i] == num)
-        {
-            count++;
-        }
+// brute force approach
+int f(int arr[], int num) {
+  int count = 0;
+  for (int i = 0; i < 5; i++) {
+    if (arr[i] == num) {
+      count++;
     }
-
-    return count;
+  }
+  return count;
 }
 
-int main()
-{
-    int size;
-    int a[size];
-    cout << "Enter the number of elements: ";
-    cin >> size;
-    cout << endl;
+int main() {
+  int size;
+  int a[size];
+  cout << "Enter the number of elements: ";
+  cin >> size;
+  cout << endl;
 
-    // Input array elements
-    cout << "Enter array elements:" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        cin >> a[i];
-    }
+  // Input array elements
+  cout << "Enter array elements:" << endl;
+  for (int i = 0; i < size; i++) {
+    cin >> a[i];
+  }
 
-    cout << " Enter the number to be searched for: ";
-    int x;
-    cin >> x;
-    // Function call
-    cout << "Number of times the number 4 appears in array is: " << f(a, x);
+  cout << " Enter the number to be searched for: ";
+  int x;
+  cin >> x;
+  // Function call
+  cout << "Number of times the number 4 appears in array is: " << f(a, x);
 
-    return 0;
+  return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,76 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-class organisms{
+// Node class definition
+class Node {
+public:
+    int data; // Data stored in the node
+    Node* next; // Pointer to the next node in the list
 
-    string living;
-    string nonliving;
+    // Constructor that initializes a node with data and sets next to nullptr
+    //Node(int data) : data(data), next(nullptr) {}
+    
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
 
+    // Constructor that initializes a node with data and a pointer to the next node
+    //Node(int data, Node* next) : data(data), next(next) {}
+    
+    Node(int data, Node* next){
+        this->data = data;
+        this->next = next;
+    }
 };
 
-int main(){
+// Function to append a new node with the given data to the end of the list
+Node* append(Node* head, int data) {
+    // If the list is empty, create a new node and return it
+    if (head == nullptr) {
+        return new Node(data);
+    }
+
+    // Create a new node to be added at the end of the list
+    Node* newNode = new Node(data);
+
+    // Traverse the list to find the last node
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+
+    // Link the last node to the new node
+    current->next = newNode;
+
+    // Return the head of the list
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n; // Read the number of elements to be added to the list
+
+    Node* head = nullptr; // Initialize the head of the list to nullptr
+
+    // Read each element and append it to the list
+    while (n--) {
+        int element;
+        cin >> element;
+        head = append(head, element);
+    }
+
+    print(head);
+    // The list is now fully constructed, but we don't do anything with it in this example
 
     return 0;
 }
