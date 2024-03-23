0d129d1e5fc5ef1eb628671f4c8f6bf4589060d3
mounishvatti <mounishvatti2002@gmail.com>
2024-03-23 19:42:02 +0530

    added new code for BinarySearch

--- Binary Search/intro.cpp
+++ Binary Search/intro.cpp
@@ -1,34 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
 using namespace std;
+
+int BinarySearch(vector<int>&arr, int target){
+	int size = arr.size();
+	int low = 0;
+	int high = size - 1;
+	
+	
+	while(low <= +high){
+		int mid = (high+low)/2;
+		if(arr[mid] == target) return mid;
+		else if(target > arr[mid]) low = mid+1;
+		else high = mid - 1;
+	}
+	return -1;
+}
 
 int main(){
     int n;
+    vector<int>arr;
+    
+    cout << "Enter the number of elements in the array: ";
     cin >> n;
     
-    int arr[n];
-    cout << "Enter sorted elements of the array: ";
+    cout << "Enter elements of the array in sorted format " << endl;
+    
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    	int ele;
+    	cout << "Enter value of pos " << i+1 << " :";
+    	cin >> ele;
+    	arr.push_back(ele);
     }
-    int low = 0;
-    int high = n-1;
-    int mid = (high-low)/2;
+    
     int searchNum;
+    
     cout << "Enter the number you want to search: ";
     cin >> searchNum;
-    if(searchNum >= arr[low] && searchNum <= arr[mid]){
-        for(int i=0; i<mid; i++){
-            if(arr[i]==searchNum){
-                cout<<"found: "<<arr[i]<<endl;
-            }
-        }
+    
+    int ind = BinarySearch(arr, searchNum);
+    if(ind == -1) cout << "Element not found in the array" << endl;
+    else{
+    	cout << "The element is found at position: " << ind+1 << endl;
     }
-    if(searchNum >= arr[mid] && searchNum <= arr[high]){
-        for(int i=mid; i<high; i++){
-            if(arr[i]==searchNum){
-                cout<<"found: "<<arr[i]<<endl;
-            }
-        }
-    }
+    
+	return 0;
 }
 
---
+++ LinkedLists/SLL/circularSLL.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+	int data;
+	Node* next;
+	
+	Node(int data. Node* next){
+		this->data = data;
+		this->next = next;
+	}
+}
+
+int main(){
+	
+	return 0;
+}
