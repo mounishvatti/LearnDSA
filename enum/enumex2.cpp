4fdb848d51114ab0a502aecf269960f327b7c8a9
mounishvatti <mounishvatti2002@gmail.com>
2024-03-29 23:03:35 +0530

    added one more example of enum

---
+++ enum/enumex2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    enum Level{
+        Low = 25,
+        Medium = 50,
+        High = 100
+    };
+
+    Level myLevel = Medium;
+    cout << myLevel;
+    return 0;
+}
+
