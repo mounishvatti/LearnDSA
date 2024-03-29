66038f5285c11a33cab506117ec48a5783c394a9
mounishvatti <mounishvatti2002@gmail.com>
2024-03-29 21:53:51 +0530

    added enum concept

---
+++ enum/enum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    enum Level{
+        Low,
+        Medium,
+        High
+    };
+
+    Level myLevel = Medium;
+    cout << myLevel;
+    return 0;
+}
+
