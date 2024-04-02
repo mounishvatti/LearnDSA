1bef670e126858c9f7d85908217d3ade3f5c1be8
mounishvatti <mounishvatti2002@gmail.com>
2024-04-02 23:50:34 +0530

    Added postorder, preorder & inorder traversal in a tree

--- Trees/basics.cpp
+++ Trees/basics.cpp
@@ -14,21 +14,6 @@ struct Node{
     }
 };
 
-/*
-class Node{
-public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int data){
-        this->data = data;
-        this->left = NULL;
-        this->right = NULL;
-    }
-};
-*/
-
 int main(){
     Node *root = new Node(1);
     root->left = new Node(2);
--- Trees/traversal.cpp
+++ Trees/traversal.cpp
@@ -22,10 +22,34 @@ void preorder(Node *node) {
     preorder(node->right);
 }
 
+void postorder(Node *node) {
+    if (node == NULL)
+        return;
+
+    postorder(node->left);
+    postorder(node->right);
+    cout << node->data << " ";
+}
+
+void inorder(Node *node){
+    if(node == NULL)
+        return;
+    
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+}
+
 int main(){
     Node *node = new Node(1);
     node->left = new Node(2);
     node->right = new Node(3);
     node->left->left = new Node(4);
     node->left->right = new Node(5); 
+
+    inorder(node);
+    cout << endl;
+    preorder(node);
+    cout << endl;
+    postorder(node);
 }
