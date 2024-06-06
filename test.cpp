ee0053bec9b1e7a980877effc75218167e906baa
mounishvatti <mounishvatti2002@gmail.com>
2024-06-06 23:21:48 +0530

    initial commit

--- test.cpp
+++ test.cpp
@@ -1,76 +1,26 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-// Node class definition
-class Node {
-public:
-    int data; // Data stored in the node
-    Node* next; // Pointer to the next node in the list
-
-    // Constructor that initializes a node with data and sets next to nullptr
-    //Node(int data) : data(data), next(nullptr) {}
-    
-    Node(int data){
-        this->data = data;
-        this->next = nullptr;
+namespace Foo // define a namespace named Foo
+{
+    // This doSomething() belongs to namespace Foo
+    int doSomething(int x, int y)
+    {
+        return x + y;
     }
-
-    // Constructor that initializes a node with data and a pointer to the next node
-    //Node(int data, Node* next) : data(data), next(next) {}
-    
-    Node(int data, Node* next){
-        this->data = data;
-        this->next = next;
-    }
-};
-
-// Function to append a new node with the given data to the end of the list
-Node* append(Node* head, int data) {
-    // If the list is empty, create a new node and return it
-    if (head == nullptr) {
-        return new Node(data);
-    }
-
-    // Create a new node to be added at the end of the list
-    Node* newNode = new Node(data);
-
-    // Traverse the list to find the last node
-    Node* current = head;
-    while (current->next != nullptr) {
-        current = current->next;
-    }
-
-    // Link the last node to the new node
-    current->next = newNode;
-
-    // Return the head of the list
-    return head;
 }
 
-void print(Node* head){
-    Node* temp = head;
-    while(temp){
-        cout << temp->data << " -> ";
-        temp = temp->next;
+namespace Goo // define a namespace named Goo
+{
+    // This doSomething() belongs to namespace Goo
+    int doSomething(int x, int y)
+    {
+        return x - y;
     }
-    cout << endl;
 }
 
-int main() {
-    int n;
-    cin >> n; // Read the number of elements to be added to the list
-
-    Node* head = nullptr; // Initialize the head of the list to nullptr
-
-    // Read each element and append it to the list
-    while (n--) {
-        int element;
-        cin >> element;
-        head = append(head, element);
-    }
-
-    print(head);
-    // The list is now fully constructed, but we don't do anything with it in this example
-
+int main()
+{
+    std::cout << Foo::doSomething(4, 3) << '\n'; // use the doSomething() that exists in namespace Foo
+    std::cout << Goo::doSomething(4, 3) << '\n';
     return 0;
 }
