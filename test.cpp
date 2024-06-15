4817dcc12c36c49b98b42fba8561a80926950a89
mounishvatti <mounishvatti2002@gmail.com>
2024-06-15 10:08:35 +0530

    linkedlist practice

--- test.cpp
+++ test.cpp
@@ -1,26 +1,124 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-namespace Foo // define a namespace named Foo
-{
-    // This doSomething() belongs to namespace Foo
-    int doSomething(int x, int y)
-    {
-        return x + y;
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int data, Node* next = nullptr){
+        this->data = data;
+        this->next = next;
     }
+};
+
+Node* arr2LinkedL(vector<int>&arr){
+    if(arr.empty()){
+        return nullptr;
+    }
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
 }
 
-namespace Goo // define a namespace named Goo
-{
-    // This doSomething() belongs to namespace Goo
-    int doSomething(int x, int y)
-    {
-        return x - y;
+Node* addAtPos(Node* head, int data, int pos){
+    if(pos == 0){
+        Node* newNode = new Node(data);
+        newNode->next=head;
+        return newNode;
     }
+
+    if(head == NULL && pos != 0){
+        cout << "Invalid position!"<< endl;
+        return head;
+    }
+
+    Node* temp = head;
+    int count = 1;
+    while(temp->next!=NULL && count < pos-1){
+        temp = temp->next;
+        count++;
+    }
+
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
+    
+    return head;
 }
 
-int main()
-{
-    std::cout << Foo::doSomething(4, 3) << '\n'; // use the doSomething() that exists in namespace Foo
-    std::cout << Goo::doSomething(4, 3) << '\n';
+Node* addAtfirst(Node* head, int data){
+
+    if(head == NULL){
+        Node* temp = new Node(data); 
+        head = temp;
+    }
+
+    else{
+        Node* temp = new Node(data); 
+        temp->next = head;
+        head = temp;
+    }
+
+    return head;
+}
+
+Node* addAtlast(Node* head, int data){
+    
+    if(head == NULL){
+        Node* temp = new Node(data); 
+        head = temp;
+    }
+
+    else{
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        Node* newNode = new Node(data);
+        temp->next = newNode;
+    }
+    return head;
+}
+
+Node* append(Node* head, int data){
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    Node* newNode = new Node(data);
+    temp->next = newNode;
+    
+    return head;
+}
+
+void display(Node* head){
+    Node* temp = head;
+    if(temp==NULL){
+        cout << "The linked list is empty!";
+        return;
+    }
+
+    while(temp!=NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+
+    cout << endl;
+}
+
+int main(){
+    vector<int>arr = {1,2,3,4,5,6,7,8,9};
+    Node* head = arr2LinkedL(arr);
+    display(head);
+    Node* newList = addAtPos(head, 10, 5);
+    display(head);
     return 0;
 }
