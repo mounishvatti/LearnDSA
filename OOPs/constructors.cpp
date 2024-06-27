c47043c051b0f0685c4ffc3f1cf1a44df73bca26
mounishvatti <mounishvatti2002@gmail.com>
2024-06-27 13:02:59 +0530

    deleting a constructor

--- OOPs/constructors.cpp
+++ OOPs/constructors.cpp
@@ -1,24 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Test{
+class Test
+{
 
     int val;
     char a;
 
-    public:
-        Test(){
-            cout << "Constructor called" << endl;
-        }
-        ~Test(){
-            cout << "Destructor called" << endl;
-        }
+public:
+    Test()
+    {
+        cout << "Constructor called" << endl;
+    }
+    ~Test()
+    {
+        cout << "Destructor called" << endl;
+    }
 };
 
-int main(){
+int main()
+{
     Test t1;
 
-    Test* t2 = new Test();
+    Test *t2 = new Test();
 
     delete t2;
 }
+
+// imagine we want to not let any user create an object of a class we can do it in 2 ways
+
+/*
+
+class Test
+{
+private:
+    Test()
+    {
+        cout << "Constructor called" << endl;
+    }
+};
+
+*/
+
+
+// or
+
+/*
+
+class Test
+{
+    Test() = delete;
+};
+
+*/
